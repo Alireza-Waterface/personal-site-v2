f98e9d653b6183ce5fde08c5ca7f2435e109135c
Alireza Waterface <Alireza.waterface@outlook.com>
2025-12-24 16:48:57 +0330

    height fixed - removed unused code

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,11 @@
-import { getDictionary, Locale } from "@/lib/getDictionary";
+import { Locale } from "@/lib/getDictionary";
 import Introduce from "./Introduce";
 import Photo from "./Photo";
 import ParticleBackground from "./ParticleBackground";
 
 export default async function Header({ lang }: { lang: Locale }) {
-   const dict = await getDictionary(lang);
-
    return (
-      <header className="flex flex-col lg:flex-row gap-8 items-center justify-between h-[85vh] px-4 relative">
+      <header className="flex flex-col lg:flex-row gap-8 items-center justify-between h-[calc(100vh-86px)] px-4 relative">
          <ParticleBackground />
          <Introduce lang={lang} />
          <Photo />
